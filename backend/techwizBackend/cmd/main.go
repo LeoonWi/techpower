81d8f45b604a20d8e1021bee40b8f47d7d877045
Виталий Шагин <shagin.v.i.21@gmail.com>
2025-06-19 14:19:55 +0300

    init mongodb

--- backend/techwizBackend/cmd/main.go
+++ backend/techwizBackend/cmd/main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"github.com/labstack/echo/v4"
+	"techwizBackend/pkg/repository"
 	"techwizBackend/pkg/transport/http"
 )
 
 func main() {
 	e := echo.New()
+	repository.New()
 	http.New(e)
 	err := e.Start(":8080")
 	if err != nil {
---
+++ backend/techwizBackend/pkg/repository/InitDB.go
@@ -0,0 +1,24 @@
+package repository
+
+import (
+	"context"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+	"os"
+)
+
+func New() *mongo.Client {
+	uri := os.Getenv("MONGODB_URI")
+	client, err := mongo.Connect(options.Client().ApplyURI(uri))
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		if err := client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+
+	return client
+}
