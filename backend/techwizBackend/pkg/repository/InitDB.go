125d50bc089362a476614c3adcf7642b19847335
Виталий Шагин <shagin.v.i.21@gmail.com>
2025-06-19 16:34:10 +0300

    fix incorrect URI mongodb

--- backend/techwizBackend/pkg/repository/InitDB.go
+++ backend/techwizBackend/pkg/repository/InitDB.go
@@ -2,13 +2,19 @@ package repository
 
 import (
 	"context"
+	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/v2/mongo"
 	"go.mongodb.org/mongo-driver/v2/mongo/options"
+	"log"
 	"os"
 )
 
 func New() *mongo.Client {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
 	uri := os.Getenv("MONGODB_URI")
+	log.Println(uri)
 	client, err := mongo.Connect(options.Client().ApplyURI(uri))
 	if err != nil {
 		panic(err)
